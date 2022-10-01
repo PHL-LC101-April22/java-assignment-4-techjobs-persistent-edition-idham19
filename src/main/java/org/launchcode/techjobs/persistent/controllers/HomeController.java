d7824eeaff98552a349eff2ed1f3e85133c0ee4f
mahdi <mahdibelabbas@gmail.com>
2022-10-01 18:45:24 -0400

    MT

--- src/main/java/org/launchcode/techjobs/persistent/controllers/HomeController.java
+++ src/main/java/org/launchcode/techjobs/persistent/controllers/HomeController.java
@@ -55,8 +55,7 @@ public class HomeController {
         }
         List<Skill> skillObjs = (List<Skill>) skillRepository.findAllById(skills);
         newJob.setSkills(skillObjs);
-//        jobRepository.save(newJob);
-//        employerRepository.save(employerId);
+
         return "redirect:";
     }
 
